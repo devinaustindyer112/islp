744ea2edd8ea376f7b5851f5c30a4d1474788761
Devin Dyer <dyerda112@gmail.com>
2025-01-16 09:06:38 -0500

    more messing around

--- hello.py
+++ hello.py
@@ -1,21 +1,18 @@
 import numpy as np
 from matplotlib.pyplot import subplots
+import matplotlib.pyplot as plt
 
-rng = np.random.default_rng(3)
-x = rng.standard_normal(100) 
-y = rng.standard_normal(100) 
+A = np.arange(16).reshape((4,4))
 
-fig, ax = subplots(figsize=(8, 8))
-x = np.linspace(-np.pi, np.pi, 50)
-y=x
-f = np.multiply.outer(np.cos(y), 1 / (1 + x**2)) 
-ax.contour(x, y, f, levels=100)
+keep_rows = np.zeros(A.shape[0], bool)
+keep_rows[[1,3]] = True
 
-A1 = np.arange(16).reshape(4,4)
-print(A1)
+print(np.array([0,1,0,1]))
+print(np.array([0,1,0,1]) == keep_rows)
 
-A2 = np.arange(16).reshape(4,4)
-print(A2)
+fig, ax = subplots(figsize=(4,4))
+ax.plot([1,1,0], [0,1,1])
+plt.show()
 
 # https://stackoverflow.com/questions/49944871/deactivate-a-pipenv-environment/51075851#51075851
 
